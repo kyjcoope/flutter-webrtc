93d88596a1b37d7c21b46e02ca82407901bb68bb
kyjcoope <51489123+kyjcoope@users.noreply.github.com>
2025-03-31 13:29:15 -0400

    Update CustomVideoDecoderFactory.mm

--- ios/Classes/CustomVideoDecoderFactory.mm
+++ ios/Classes/CustomVideoDecoderFactory.mm
@@ -8,7 +8,7 @@
 static NSMutableArray<NSString *> *trackQueue;
 
 + (void)initialize {
-    NSLog(@"SuperDecoder: initialize");
+    NSLog(@"CustomVideoDecoderFactory: initialize");
     if (self == [CustomVideoDecoderFactory class]) {
         trackQueue = [NSMutableArray array];
     }
@@ -16,29 +16,28 @@ static NSMutableArray<NSString *> *trackQueue;
 
 + (void)setTrackId:(NSString *)trackId {
     @synchronized(trackQueue) {
-        NSLog(@"SuperDecoder: Adding trackId to queue: %@", trackId);
+        NSLog(@"CustomVideoDecoderFactory: Adding trackId to queue: %@", trackId);
         [trackQueue addObject:trackId];
     }
 }
 
 - (instancetype)init {
-    NSLog(@"SuperDecoder: init");
+    NSLog(@"CustomVideoDecoderFactory: init");
     self = [super init];
     return self;
 }
 
 - (id<RTCVideoDecoder>)createDecoder:(RTCVideoCodecInfo *)info {
-    NSLog(@"SuperDecoder: Creating decoder for codec: %@", info.name);
-    NSLog(@"SuperDecoder: Codec parameters: %@", info.parameters);
+    NSLog(@"CustomVideoDecoderFactory: Creating decoder for codec: %@", info.name);
     NSString *trackId = nil;
     
     @synchronized(trackQueue) {
         if (trackQueue.count > 0) {
             trackId = trackQueue[0];
             [trackQueue removeObjectAtIndex:0];
-            NSLog(@"SuperDecoder: Creating decoder with trackId: %@", trackId);
+            NSLog(@"CustomVideoDecoderFactory: Creating decoder with trackId: %@", trackId);
         } else {
-            NSLog(@"SuperDecoder: Warning: Creating decoder with no associated trackId");
+            NSLog(@"CustomVideoDecoderFactory: Warning: Creating decoder with no associated trackId");
         }
     }
     return [[RTCVideoDecoderBypass alloc] initWithTrackId:trackId codecInfo:info];
